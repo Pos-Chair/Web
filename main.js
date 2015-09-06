067dfe2b93beef1438363b88171df63acfdac229
Zachary Kaplan <zacharytrent@gmail.com>
2015-09-06 01:19:24 +0000

    Working on User Auth, more dev on the static page

--- main.js
+++ main.js
@@ -5,61 +5,77 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var ns = require('node-session');
+var spark = require('spark');
+
+var alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+var secret = '';
+for(var i = 0; i < 32; ++i) {
+    secret += alpha.charAt(Math.floor(Math.random()*alpha.length));
+}
+
+var sess = new ns({secret: secret, secure: true, encrypt: true});
+console.log('Sessions Manager Created with secret: ' + secret);
 
 http.createServer(function (req, response) {
 
-    var pathname = url.parse(req.url).pathname;
+    sess.startSession(req, response, function() {
 
-    console.log("Processing request for " + pathname + ".");
+        var pathname = url.parse(req.url).pathname;
 
-    if(pathname === '/')
-        pathname += 'index.html';
+        console.log("Processing request for " + pathname + ".");
 
-    var ext = pathname.substr(pathname.lastIndexOf('.')+1);
-    var filename = 'web' + pathname;
+        if(pathname === '/')
+            pathname += 'index.html';
 
-    fs.stat(filename, function(err, stat) {
-        if(err) {
-            console.error(err);
-            response.writeHead(404, {'Context-Type': 'text/html'});
-        } else {
-            var type;
-            switch(ext) {
-            case 'htm':
-            case 'html':
-                type = 'text/html';
-                break;
-            
-            case 'css':
-                type = 'stylesheet';
-                break;
+        var ext = pathname.substr(pathname.lastIndexOf('.')+1);
+        var filename = 'web' + pathname;
 
-            case 'js':
-                type = 'script';
-                break;
-
-            case 'ico':
-                type = 'x-icon';
-                break;
-
-            default:
-                type = ext;
-            }
-            response.writeHead(200, {'Context-Type': type});
-            
-            var stream = fs.createReadStream(filename);
-            
-            stream.on('open', function() {
-                stream.pipe(response);
-            });
-            stream.on('error', function(err) {
-                response.end(err);
+        fs.stat(filename, function(err, stat) {
+            if(err) {
                 console.error(err);
-            });
-            stream.on('end', function() {
-                response.end();
-            });
-        }
+                response.writeHead(404, {'Context-Type': 'text/html'});
+            } else {
+                var type;
+                switch(ext) {
+                case 'htm':
+                case 'html':
+                    type = 'text/html';
+                    break;
+                
+                case 'css':
+                    type = 'stylesheet';
+                    break;
+
+                case 'js':
+                    type = 'script';
+                    break;
+
+                case 'ico':
+                    type = 'x-icon';
+                    break;
+
+                default:
+                    type = ext;
+                }
+                response.writeHead(200, {'Context-Type': type});
+                
+                var stream = fs.createReadStream(filename);
+                
+                stream.on('open', function() {
+                    stream.pipe(response);
+                });
+                stream.on('error', function(err) {
+                    response.end(err);
+                    console.error(err);
+                });
+                stream.on('end', function() {
+                    response.end();
+                });
+            }
+
+        });
+
 
     });
 
